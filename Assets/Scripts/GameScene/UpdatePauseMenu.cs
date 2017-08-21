4df1b6245451177449105a57cfed92a9cc49e2a6
NinjaNH99 <31159802+NinjaNH99@users.noreply.github.com>
2017-08-21 16:23:12 +0300

    Build 0.1

---
+++ Assets/Scripts/GameScene/UpdatePauseMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UpdatePauseMenu : MonoBehaviour {
+
+	public GameObject score;
+	public GameObject bonus;
+	public GameObject bestScore;
+
+	public void UpdateGameStatus()
+	{
+		
+	}
+	
+}
