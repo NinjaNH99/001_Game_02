789d893c98e710646709b9b90d27b2b9fa777b19
NinjaNH99 <31159802+NinjaNH99@users.noreply.github.com>
2017-08-25 23:52:21 +0300

    Build 0.36

--- Assets/Scripts/GameScene/LevelContainer.cs
+++ Assets/Scripts/GameScene/LevelContainer.cs
@@ -3,7 +3,7 @@ using TMPro;
 
 public class LevelContainer : MonoSingleton<LevelContainer>
 {
-    private const float DISTANCE_BETWEEN_BLOCKS = 0.85f; // 0.35
+    private const float DISTANCE_BETWEEN_BLOCKS = 85.0f; // 0.35
 
     public GameObject rowPrefab;
     public GameObject containerPrefab;
@@ -19,11 +19,13 @@ public class LevelContainer : MonoSingleton<LevelContainer>
     public int nrBlocksInGame;
 
     private int e;
+    private bool animPosY;
 
     private void Awake()
     {
+        animPosY = true;
         //gameObject.transform.position = new Vector3(0, 1.3f, 0);
-        GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -200);
+        GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -200.0f);
         rowContainerStartingPosition = rowContainer.anchoredPosition;
         desiredPosition = rowContainerStartingPosition;
         lastBallSpawn = 1f;
@@ -39,24 +41,31 @@ public class LevelContainer : MonoSingleton<LevelContainer>
     }
 
     private void Update()
-    { 
-        if ((Vector2)rowContainer.anchoredPosition != desiredPosition)
-            rowContainer.position = Vector3.MoveTowards(rowContainer.anchoredPosition, desiredPosition + new Vector2(0, DISTANCE_BETWEEN_BLOCKS), Time.deltaTime);
+    {
+        if (animPosY)
+        {
+            if ((Vector2)rowContainer.anchoredPosition != desiredPosition)
+                rowContainer.anchoredPosition = Vector3.MoveTowards(rowContainer.anchoredPosition, desiredPosition + new Vector2(0, DISTANCE_BETWEEN_BLOCKS), Time.deltaTime * 200);
+            if ((Vector2)rowContainer.anchoredPosition == desiredPosition + new Vector2(0, DISTANCE_BETWEEN_BLOCKS))
+                animPosY = false;
+        }
     }
 
     public void GenerateNewRow()
     {
+        animPosY = true;
         bool doNotSpawn = false;
         bonus_01Text.gameObject.SetActive(false);
         GameObject go = Instantiate(rowPrefab, rowContainer) as GameObject;
         go = GenerateRowBlocks(go);
         //go.transform.localPosition = Vector2.down * currentSpawnY;
         //go.GetComponent<RectTransform>().localPosition = new Vector2(0, -235) * currentSpawnY;
-        go.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -85) * currentSpawnY;
+        go.GetComponent<RectTransform>().localPosition = Vector2.down * currentSpawnY;
 
         currentSpawnY -= DISTANCE_BETWEEN_BLOCKS;
 
-        desiredPosition = rowContainerStartingPosition + (Vector2.up * currentSpawnY);
+        //desiredPosition = rowContainerStartingPosition + (Vector2.up * currentSpawnY);
+        desiredPosition = rowContainerStartingPosition + Vector2.up * currentSpawnY;
 
         Container[] blockArray = go.GetComponentsInChildren<Container>();
 
@@ -115,7 +124,7 @@ public class LevelContainer : MonoSingleton<LevelContainer>
 
     private GameObject GenerateRowBlocks(GameObject row)
     {
-        int[] posArray = new int[7];
+        int[] posArray = { 5, 5, 5, 5, 5, 5, 5 };
         int pos;
         bool posIsClean = false;
         int nrBlocks = Random.Range(4, 7);
@@ -128,7 +137,9 @@ public class LevelContainer : MonoSingleton<LevelContainer>
                 for (int e = 0; e < posArray.Length; e++)
                 {
                     if (pos != posArray[e])
+                    {
                         posIsClean = true;
+                    }
                     else
                     {
                         posIsClean = false;
@@ -137,8 +148,9 @@ public class LevelContainer : MonoSingleton<LevelContainer>
                 }
             } while (!posIsClean);
             posArray[i] = pos;
-            go.transform.position = new Vector2(pos * 0.35f, row.transform.position.y);
-            posIsClean = true;
+            Debug.Log(pos);
+            //go.transform.position = new Vector2(pos * 0.35f, row.transform.position.y);
+            go.GetComponent<RectTransform>().anchoredPosition = new Vector2(pos * DISTANCE_BETWEEN_BLOCKS, row.GetComponent<RectTransform>().anchoredPosition.y);
         }
         nrBlocksInGame += nrBlocks;
         return row;
